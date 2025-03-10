8e4e35979ebfaf97c8ae7d0d2217b0e8876b5039
meiliamyy <meiliarahmi8@gmail.com>
2025-03-10 13:56:19 +0700

    add new modified invoice.js SQL project

--- invoice.js
+++ invoice.js
@@ -10,8 +10,64 @@ export default function registerInvoice(fastify, opts, done) {
     const id = request.query.id;
 
     // code goes here
+    db.all(
+      `SELECT 
+        InvoiceId as id,
+        InvoiceDate as date, 
+        BillingAddress as address, 
+        BillingCity as city, 
+        BillingState as state, 
+        BillingCountry as country, 
+        BillingPostalCode AS postalCode, 
+        Total as total 
+      FROM 
+        Invoice
+      WHERE 
+        InvoiceId = ?`,
+      [id],
+      (err, invoiceRows) => {
+        if (err) {
+          reply.code(500);
+          reply.send({ error: err, errorLocation: "Invoice" });
+          return;
+        }
 
-    reply.send({ invoice: {}, lines: [] });
+        if (invoiceRows.length === 0) {
+          reply.code(404);
+          reply.send({ error: "Not found" });
+          return;
+        }
+
+        db.all(
+          `SELECT 
+            i.UnitPrice AS unitPrice, 
+            i.Quantity AS quantity, 
+            t.Name AS trackName, 
+            a.Title AS albumTitle,
+            ar.Name AS artistName
+          FROM 
+            InvoiceLine i
+          JOIN
+            Track t ON t.TrackId = i.TrackId
+          JOIN
+            Album a ON a.AlbumId = t.AlbumId
+          JOIN
+            Artist ar ON ar.ArtistId = a.ArtistId
+          WHERE 
+            InvoiceId = ?`,
+          [id],
+          (err, trackRows) => {
+            if (err) {
+              reply.code(500);
+              reply.send({ error: err, errorLocation: "Track" });
+              return;
+            }
+
+            reply.send({ invoice: invoiceRows[0], lines: trackRows });
+          }
+        );
+      }
+    );
   });
 
   done();
